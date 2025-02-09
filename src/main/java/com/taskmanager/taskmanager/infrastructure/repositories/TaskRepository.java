c5bfaf12f59c7c35f2258ed41f1155bbb75661e6
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2025-02-09 16:23:17 -0300

    feat: desenvolvimento repository para user e task

--- src/main/java/com/taskmanager/taskmanager/infrastructure/repositories/TaskRepository.java
+++ src/main/java/com/taskmanager/taskmanager/infrastructure/repositories/TaskRepository.java
@@ -1,4 +1,11 @@
 package com.taskmanager.taskmanager.infrastructure.repositories;
 
-public class TaskRepository {
+import com.taskmanager.taskmanager.domain.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findUserById(Long userId);
 }
--- src/main/java/com/taskmanager/taskmanager/infrastructure/repositories/UserRepository.java
+++ src/main/java/com/taskmanager/taskmanager/infrastructure/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.taskmanager.taskmanager.infrastructure.repositories;
 
-public class UserRepository {
+import com.taskmanager.taskmanager.domain.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }

b32fae29e60b6a6e1442fcd53f124e7a087c0a81
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2025-02-09 16:16:28 -0300

    feat: desenvolvimento dos models

--- src/main/java/com/taskmanager/taskmanager/domain/models/Task.java
+++ src/main/java/com/taskmanager/taskmanager/domain/models/Task.java
@@ -1,4 +1,35 @@
 package com.taskmanager.taskmanager.domain.models;
 
+import com.taskmanager.taskmanager.domain.enums.TaskPriority;
+import com.taskmanager.taskmanager.domain.enums.TaskStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskPriority priority;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
+

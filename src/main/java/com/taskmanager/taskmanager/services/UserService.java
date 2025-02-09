fbf89313666eba406252a54e56b7fe0fa331e1e5
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2025-02-09 17:03:23 -0300

    feat: desenvolvimento do UserService

--- src/main/java/com/taskmanager/taskmanager/services/UserService.java
+++ src/main/java/com/taskmanager/taskmanager/services/UserService.java
@@ -1,4 +1,51 @@
 package com.taskmanager.taskmanager.services;
 
+import com.taskmanager.taskmanager.application.dto.UserDTO;
+import com.taskmanager.taskmanager.application.exceptions.ResourceNotFoundException;
+import com.taskmanager.taskmanager.domain.models.User;
+import com.taskmanager.taskmanager.infrastructure.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(UserDTO userDTO){
+        if(userRepository.findByEmail(userDTO.email()).isPresent()){
+            throw new RuntimeException("E-mail já cadastrado!");
+        }
+
+        User user = new User();
+        user.setEmail(userDTO.email());
+        user.setPassword(passwordEncoder.encode(userDTO.password()));
+        user.setUsername(userDTO.username());
+
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long userId) {
+        if(!userRepository.existsById(userId)){
+            throw new ResourceNotFoundException("Usuário não encontrado");
+        }
+        userRepository.deleteById(userId);
+    }
 }
